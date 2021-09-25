76ca1c0f7fd0b33f3d697fc1da28d9303e2c166f
anandkumarcoder <akanand104@gmail.com>
2021-09-25 09:01:56 +0530

    clear button added , empy field check

--- app.js
+++ app.js
@@ -6,21 +6,32 @@ const subtractBtn = document.querySelector('.subtract');
 const multiplyBtn = document.querySelector('.multiply');
 const divideBtn = document.querySelector('.divide');
 const outputDiv = document.querySelector('.output')
+const clear = document.querySelector('.clear')
 
 
 // console.log(addBtn , subtractBtn ,multiplyBtn ,divideBtn);
-
+clear.addEventListener('click' , ()=> {
+    location.reload()
+})
 
 // const  = console.log(inputOne.value)
 function addInput() {
     let numberOne = Number(inputOne.value)
     let numberTwo = Number(inputTwo.value)
 
+  
+
      sum = numberOne+ numberTwo
      outputDiv.innerText = `sum of two input is ${sum}`;
+
+     if (numberOne == '' && numberTwo == '') {
+        outputDiv.innerText = "please put in all the values"
+    }
+
+    
+
     
 }
-
 
 
 addBtn.addEventListener ('click',  addInput )
@@ -30,6 +41,8 @@ function subInput() {
     let numberOne = Number(inputOne.value)
     let numberTwo = Number(inputTwo.value)
 
+   
+
     if( numberOne > numberTwo){
         subtract = numberOne-numberTwo;
         outputDiv.innerText = `differnece between two input is ${subtract}`;
@@ -37,6 +50,10 @@ function subInput() {
     } else {
         subtract= numberTwo-numberOne;
         outputDiv.innerText = `differnece between two input is ${subtract}`;
+    }
+
+    if (numberOne == '' && numberTwo == '') {
+        outputDiv.innerText = "please put in all the values"
     }
     
 }
@@ -52,8 +69,15 @@ function multiplyInput() {
     let numberOne = Number(inputOne.value)
     let numberTwo = Number(inputTwo.value)
 
+   
+
      multiply = numberOne * numberTwo;
      outputDiv.innerText = `product of two input is ${multiply}`;
+
+     if (numberOne == '' && numberTwo == '') {
+        outputDiv.innerText = "please put in all the values"
+
+    }
     
 }
 
@@ -66,9 +90,16 @@ function divideInput() {
     let numberOne = Number(inputOne.value)
     let numberTwo = Number(inputTwo.value)
 
+   
+
      divide = numberOne/numberTwo;
      outputDiv.innerText = `division of two input is ${divide}`;
-    
+
+     
+     if (numberOne == '' && numberTwo == '') {
+        outputDiv.innerText = "please put in all the values"
+
+    }
 }
 
 
